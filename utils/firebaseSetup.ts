9f47d233519e0e93dc0149ef6a03ed353a4ade40
Stan Chen <stanchensz@gmail.com>
2024-06-20 23:29:27 -0600

    firebase setup

--- utils/firebaseSetup.ts
+++ utils/firebaseSetup.ts
@@ -1,28 +1,30 @@
-// firebaseSetup.ts
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, signOut } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCz2uQXGeX9l1Y-vb6tgY5hlTTpJ-ZUZ7Y",
-  authDomain: "techcademy-fa5ae.firebaseapp.com",
-  projectId: "techcademy-fa5ae",
-  storageBucket: "techcademy-fa5ae.appspot.com",
-  messagingSenderId: "415322424704",
-  appId: "1:415322424704:web:d49a001d79990069e82d65",
-  measurementId: "G-7XHRR4XNR2"
+  apiKey: "AIzaSyB07Y42LZ7QCFwOWLdjGjfIOOHTY6B35Ho",
+  authDomain: "techcademy-a5928.firebaseapp.com",
+  projectId: "techcademy-a5928",
+  storageBucket: "techcademy-a5928.appspot.com",
+  messagingSenderId: "352867732758",
+  appId: "1:352867732758:web:c482427596906a1456d437"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
 
 export const signUp = async (email: string, password: string) => {
   try {
     await createUserWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    console.error('Error signing up: ', error);
+    console.error('Error signing up:', error);
+    throw error;
   }
 };
 
@@ -30,7 +32,8 @@ export const signIn = async (email: string, password: string) => {
   try {
     await signInWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    console.error('Error signing in: ', error);
+    console.error('Error signing in:', error);
+    throw error;
   }
 };
 
@@ -38,7 +41,8 @@ export const resetPassword = async (email: string) => {
   try {
     await sendPasswordResetEmail(auth, email);
   } catch (error) {
-    console.error('Error resetting password: ', error);
+    console.error('Error resetting password:', error);
+    throw error;
   }
 };
 
@@ -47,6 +51,7 @@ export const logOut = async () => {
     await signOut(auth);
   } catch (error) {
     console.error('Error signing out: ', error);
+    throw error;
   }
 };
 
