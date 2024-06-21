2e20db7b7b4a237a93e815da2247101df4ce1095
Stan Chen <stanchensz@gmail.com>
2024-06-21 00:26:39 -0600

    modified styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ export default function Layout({ children }) {
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // Bootstrap's JS needs to be initialized only in the browser environment
       require('bootstrap/dist/js/bootstrap.bundle.min');
     }
   }, []);
@@ -17,13 +16,12 @@ export default function Layout({ children }) {
     <div className="d-flex flex-column vh-100">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#" onClick={() => router.push('/')}> 
+          <a className="navbar-brand" href="#" onMouseOver={(e) => { (e.target as HTMLElement).style.opacity = '0.7'; }} onMouseOut={(e) => { (e.target as HTMLElement).style.opacity = '1'; }} onClick={() => router.push('/')}> 
             <img src="/logo.png" alt="Logo" width="120" height="45" className="d-inline-block align-top" />
           </a>
-          <button className="btn btn-primary me-3" onClick={() => router.push('/')}>Home</button>
           <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-              User
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="userDropdown" data-bs-toggle="dropdown" aria-expanded="false" style={{backgroundColor: '#4CAF50'}}>
+            SignUp / Login
             </button>
             <ul className="dropdown-menu" aria-labelledby="userDropdown">
               <li><a className="dropdown-item" href="#" onClick={() => router.push('/login')}>Login</a></li>
